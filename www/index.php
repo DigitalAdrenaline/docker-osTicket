52ab54fe981eb5cb5be11aa1a99a13f31ce99ad6
Thomas Peters <thomas.peters@sirmysterion.com>
2022-10-03 15:04:26 -0600

    Added osTicket to WWW, Update Dockerfile to include extra extentions

--- www/index.php
+++ www/index.php
@@ -1,66 +1,82 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>LAMP STACK</title>
-        <link rel="stylesheet" href="/assets/css/bulma.min.css">
-    </head>
-    <body>
-        <section class="hero is-medium is-info is-bold">
-            <div class="hero-body">
-                <div class="container has-text-centered">
-                    <h1 class="title">
-                        LAMP STACK
-                    </h1>
-                    <h2 class="subtitle">
-                        Your local development environment
-                    </h2>
-                </div>
-            </div>
-        </section>
-        <section class="section">
-            <div class="container">
-                <div class="columns">
-                    <div class="column">
-                        <h3 class="title is-3 has-text-centered">Environment</h3>
-                        <hr>
-                        <div class="content">
-                            <ul>
-                                <li><?= apache_get_version(); ?></li>
-                                <li>PHP <?= phpversion(); ?></li>
-                                <li>
-                                    <?php
-                                    $link = mysqli_connect("database", "root", $_ENV['MYSQL_ROOT_PASSWORD'], null);
+<?php
+/*********************************************************************
+    index.php
 
-/* check connection */
-                                    if (mysqli_connect_errno()) {
-                                        printf("MySQL connecttion failed: %s", mysqli_connect_error());
-                                    } else {
-                                        /* print server version */
-                                        printf("MySQL Server %s", mysqli_get_server_info($link));
-                                    }
-                                    /* close connection */
-                                    mysqli_close($link);
-                                    ?>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="column">
-                        <h3 class="title is-3 has-text-centered">Quick Links</h3>
-                        <hr>
-                        <div class="content">
-                            <ul>
-                                <li><a href="/phpinfo.php">phpinfo()</a></li>
-                                <li><a href="http://localhost:<? print $_ENV['PMA_PORT']; ?>">phpMyAdmin</a></li>
-                                <li><a href="/test_db.php">Test DB Connection with mysqli</a></li>
-                                <li><a href="/test_db_pdo.php">Test DB Connection with PDO</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-    </body>
-</html>
+    Helpdesk landing page. Please customize it to fit your needs.
+
+    Peter Rotich <peter@osticket.com>
+    Copyright (c)  2006-2013 osTicket
+    http://www.osticket.com
+
+    Released under the GNU General Public License WITHOUT ANY WARRANTY.
+    See LICENSE.TXT for details.
+
+    vim: expandtab sw=4 ts=4 sts=4:
+**********************************************************************/
+require('client.inc.php');
+
+require_once INCLUDE_DIR . 'class.page.php';
+
+$section = 'home';
+require(CLIENTINC_DIR.'header.inc.php');
+?>
+<div id="landing_page">
+<?php include CLIENTINC_DIR.'templates/sidebar.tmpl.php'; ?>
+<div class="main-content">
+<?php
+if ($cfg && $cfg->isKnowledgebaseEnabled()) { ?>
+<div class="search-form">
+    <form method="get" action="kb/faq.php">
+    <input type="hidden" name="a" value="search"/>
+    <input type="text" name="q" class="search" placeholder="<?php echo __('Search our knowledge base'); ?>"/>
+    <button type="submit" class="green button"><?php echo __('Search'); ?></button>
+    </form>
+</div>
+<?php } ?>
+<div class="thread-body">
+<?php
+    if($cfg && ($page = $cfg->getLandingPage()))
+        echo $page->getBodyWithImages();
+    else
+        echo  '<h1>'.__('Welcome to the Support Center').'</h1>';
+    ?>
+    </div>
+</div>
+<div class="clear"></div>
+
+<div>
+<?php
+if($cfg && $cfg->isKnowledgebaseEnabled()){
+    //FIXME: provide ability to feature or select random FAQs ??
+?>
+<br/><br/>
+<?php
+$cats = Category::getFeatured();
+if ($cats->all()) { ?>
+<h1><?php echo __('Featured Knowledge Base Articles'); ?></h1>
+<?php
+}
+
+    foreach ($cats as $C) { ?>
+    <div class="featured-category front-page">
+        <i class="icon-folder-open icon-2x"></i>
+        <div class="category-name">
+            <?php echo $C->getName(); ?>
+        </div>
+<?php foreach ($C->getTopArticles() as $F) { ?>
+        <div class="article-headline">
+            <div class="article-title"><a href="<?php echo ROOT_PATH;
+                ?>kb/faq.php?id=<?php echo $F->getId(); ?>"><?php
+                echo $F->getQuestion(); ?></a></div>
+            <div class="article-teaser"><?php echo $F->getTeaser(); ?></div>
+        </div>
+<?php } ?>
+    </div>
+<?php
+    }
+}
+?>
+</div>
+</div>
+
+<?php require(CLIENTINC_DIR.'footer.inc.php'); ?>
